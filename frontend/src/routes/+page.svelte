009466c137dec7b783621834e45afd163b0f8b50
FerroO2000 <30318301+FerroO2000@users.noreply.github.com>
2024-07-25 18:45:18 +0200

    chore: init project

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-8xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn btn-primary">button</button>
